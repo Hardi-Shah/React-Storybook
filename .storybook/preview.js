e57b88ddd92d0556ee0cb45e703f0cce92255ca0
hardi shah <hardishahhardi@gmail.com>
2021-02-05 15:08:46 +0530

    Initial Commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,4 @@
-import { CSSReset, theme, ThemeProvider, ChakraProvider } from '@chakra-ui/react'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
-export const decorators=[
-  (Story) => (
-  <ChakraProvider theme={theme}>
-    <Story/>
-    {/* <Box m='4'><Center>{story()}</ Center></Box> */}
-    <CSSReset/>
-  </ChakraProvider>
-  )
-]
